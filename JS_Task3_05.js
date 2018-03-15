2bed8926c3795a47c95566998c853854711e2165
Philip Maksiutov <philip_bmx11@yahoo.com>
2018-03-15 14:29:04 +0300

    task 5

---
+++ JS_Task3_05.js
@@ -0,0 +1,19 @@
+var obj = {
+    name : 'Ivan',
+    age : '25',
+    height : 177,
+    gendere : 'male'
+};
+
+var newObj;
+
+
+function copyObj(obj) {
+    newObj = obj;
+    return newObj
+}
+
+
+copyObj(obj);
+
+console.log(newObj);
